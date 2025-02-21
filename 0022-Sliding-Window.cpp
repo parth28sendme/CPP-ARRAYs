9d00f251ce143b05a371f9588072536231534b79
parth28sendme <parth@sendme.today>
2025-02-21 17:05:31 +0530

    0022- Sliding window

---
+++ 0022-Sliding-Window.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std ;
+
+
+int slidingWindow(int arr[], int n , int k){
+int currentMax = 0;
+for(int i =0 ; i<k ; i++){
+currentMax = currentMax + arr[i];
+}
+
+int MainMax = currentMax;
+
+for(int i=k ; i < n ; i++){
+    currentMax = currentMax + arr[i] -arr[i-k];
+    MainMax = max(MainMax, currentMax);
+}
+
+return MainMax;
+}
+
+
+int main(){
+
+    int array[] = { 1, 8, 30, -5, 20, 7 };
+    int n = sizeof(array) / sizeof(array[0]);
+    int k = 3;
+
+    int result = 0;
+    result = slidingWindow(array,n,k);
+
+    cout << result << endl;
+
+
+
+
+    return 0;
+} 
